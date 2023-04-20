ed9351aad537261baa1219e2a35440034f046792
Akinleye Paul <paulakinleye@gmail.com>
2023-04-20 15:57:01 +0100

    made some changes

--- src/components/Home/Carousel.jsx
+++ src/components/Home/Carousel.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import sliderBg from "../../assets/slider-bg.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import awardImage from "../../assets/award.png";
 
 function CarouSel() {
   return (
     <>
-      <Carousel>
-        <div className="">
+      <Carousel
+        className="relative"
+        infiniteLoop={true}
+        autoPlay
+        showThumbs={false}
+        interval={3000}
+      >
+        <div className="relative">
+          <img src={sliderBg} alt="slider-bg"></img>
+          <div className="absolute">
+            <img
+              className="h-[20rem] w-[1rem] z-10"
+              src={awardImage}
+              alt="award"
+            ></img>{" "}
+          </div>
+        </div>
+        <div className="relative">
           <img src={sliderBg} alt="slider-bg"></img>
         </div>
-        <div>2</div>
-        <div>3</div>
+        <div className="relative">
+          <img src={sliderBg} alt="slider-bg"></img>
+        </div>
       </Carousel>
     </>
   );
